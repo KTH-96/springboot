6e4e0282397975d0c042a039e3ba88cdd24fe34a
KTH-96 <taehyun960@gmail.com>
2021-08-07 21:03:27 +0900

    jpa로 저장 테스트

---
+++ src/main/java/com/kth/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.kth.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity//기본 생성자는 필수 (JPA가 엔티티 객체 생성 시 기본 생성자를 사용)
+public class Posts {
+    @Id//해당 테이블 pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//pk생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/main/java/com/kth/book/springboot/web/HelloController.java
+++ src/main/java/com/kth/book/springboot/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.kth.book.springboot.web;
 
+import com.kth.book.springboot.web.dto.HelloResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +14,12 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
     @GetMapping("/hello-dto")
-    public String helloDto(@RequestParam String name,
-                           @RequestParam int amount) {
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
         log.info("name = {}",name);
         log.info("amount = {}",amount);
-        return "hello";
+        return new HelloResponseDto(name, amount);
     }
 }
